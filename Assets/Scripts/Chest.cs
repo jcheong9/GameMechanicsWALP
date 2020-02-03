99c8fffc2823c528fdcb419eab059da968a68392
Justin Cervantes <cervantes.jfa@gmail.com>
2020-02-02 20:19:17 -0800

    Adding prefabs of wizard and archer with colliders and animations

---
+++ Assets/Scripts/Chest.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chest : MonoBehaviour
+{
+
+    private Animator anim;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Damage()
+    {
+        anim.SetInteger("Damage", anim.GetInteger("Damage") + 5);
+        Debug.Log(anim.GetInteger("Damage"));
+        if(anim.GetInteger("Damage") > 100) {
+            gameObject.SetActive(false);
+        }
+    }
+}
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -21,6 +21,8 @@ public class PlayerMovement : MonoBehaviour
         currentState = PlayerState.walk;
         animator = GetComponent<Animator>();
         myRigidbody = GetComponent<Rigidbody2D>();
+        animator.SetFloat("moveX", 0);
+        animator.SetFloat("moveY", -1);
     }
 
     // Update is called once per frame
@@ -35,7 +37,6 @@ public class PlayerMovement : MonoBehaviour
         else if(currentState == PlayerState.walk) {
             UpdateAnimationAndMove();
         }
-        Debug.Log(currentState);
     }
 
     private IEnumerator AttackCo()
@@ -60,6 +61,7 @@ public class PlayerMovement : MonoBehaviour
     }
 
     void MoveCharacter() {
+        change.Normalize();
         myRigidbody.MovePosition(
             transform.position + change * speed * Time.deltaTime
         );
